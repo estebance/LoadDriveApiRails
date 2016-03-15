71d95770cfb64a2f08aa21f2bc10a6c6e47d3c45
estebance <restebance@gmail.com>
2016-03-15 11:01:05 -0500

    include auth button in the root page

---
+++ app/assets/javascripts/static_pages_example.js
@@ -0,0 +1,113 @@
+/**
+ * Created by estebance on 3/15/16.
+ */
+// change to js to easy integration
+
+$(document).ready(function(){
+
+    // Your Client ID can be retrieved from your project in the Google
+    // Developer Console, https://console.developers.google.com
+    var CLIENT_ID = 'your_cliend_id';
+    // what you can do with this integration
+    var SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
+
+    // copy from https://developers.google.com/drive/v3/web/quickstart/js
+
+    /**
+     * Check if current user has authorized this application.
+     */
+    function checkAuth() {
+        gapi.auth.authorize(
+            {
+                'client_id': CLIENT_ID,
+                'scope': SCOPES.join(' '),
+                'immediate': true
+            }, handleAuthResult);
+    }
+
+    /**
+     * Handle response from authorization server.
+     *
+     * @param {Object} authResult Authorization result.
+     */
+    function handleAuthResult(authResult) {
+        var authorizeDiv = document.getElementById('authorize-div');
+        if (authResult && !authResult.error) {
+            // Hide auth UI, then load client library.
+            authorizeDiv.style.display = 'none';
+            loadDriveApi();
+        } else {
+            // Show auth UI, allowing the user to initiate authorization by
+            // clicking authorize button.
+            authorizeDiv.style.display = 'inline';
+        }
+    }
+
+    /**
+     * Initiate auth flow in response to user clicking authorize button.
+     *
+     * @param {Event} event Button click event.
+     */
+    function handleAuthClick(event) {
+        gapi.auth.authorize(
+            {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
+            handleAuthResult);
+        return false;
+    }
+
+    /**
+     * Load Drive API client library.
+     */
+    function loadDriveApi() {
+        gapi.client.load('drive', 'v3', listFiles);
+    }
+
+    /**
+     * Print files.
+     */
+    function listFiles() {
+        var request = gapi.client.drive.files.list({
+            'pageSize': 10,
+            'fields': "nextPageToken, files(id, name)"
+        });
+
+        request.execute(function(resp) {
+            appendPre('Files:');
+            var files = resp.files;
+            if (files && files.length > 0) {
+                for (var i = 0; i < files.length; i++) {
+                    var file = files[i];
+                    appendPre(file.name + ' (' + file.id + ')');
+                }
+            } else {
+                appendPre('No files found.');
+            }
+        });
+    }
+
+    /**
+     * Append a pre element to the body containing the given message
+     * as its text node.
+     *
+     * @param {string} message Text to be placed in pre element.
+     */
+    function appendPre(message) {
+        var pre = document.getElementById('output');
+        var textContent = document.createTextNode(message + '\n');
+        pre.appendChild(textContent);
+    }
+
+
+    //
+    $('#authorize-button').click(function(){
+        handleAuthClick("click");
+    });
+
+    $(window).load(function() {
+        console.log(gapi);
+        checkAuth();
+    });
+
+
+
+});
